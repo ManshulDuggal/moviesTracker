23b421f65cfc14a82f0b697b6eaf6c562ede65dd
Manshul <manshuld7@gmail.com>
2021-07-18 08:30:26 +0545

    :(

--- src/components/videoModal.js
+++ src/components/videoModal.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import ModalVideo from "react-modal-video";
 import "../main.scss";
-
+import ReactHlsPlayer from "react-hls-player";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { CardHover } from "../anim/Anim";
@@ -16,9 +16,10 @@ export const VideoModal = () => {
         channel="youtube"
         autoplay
         isOpen={isOpen}
-        videoId="L61p2uyiMSo"
+        videoId="DGQwd1_dpuc"
         onClose={() => setOpen(false)}
       />
+
       <Preview>
         <motion.button
           variants={CardHover}
@@ -26,7 +27,7 @@ export const VideoModal = () => {
           className="preview"
           onClick={() => setOpen(true)}
         >
-          VIEW DEMO
+          Preview
         </motion.button>
       </Preview>
     </React.Fragment>
@@ -34,27 +35,29 @@ export const VideoModal = () => {
 };
 
 const Preview = styled.div`
-.preview {
-  border-bottom-left-radius: 1rem;
-  border-top-left-radius: 0rem;
-  border-bottom-right-radius: 1rem;
-  border-top-right-radius: 0rem;
-  color: white;
-  height: 30px;
-  background-color: #00000060;
-  width: 100%;
-  overflow: hidden;
-  border: none;
-  &:hover {
+  .preview {
     border-bottom-left-radius: 1rem;
-    border-bottom-right-radius: 1rem;
     border-top-left-radius: 0rem;
+    border-bottom-right-radius: 1rem;
     border-top-right-radius: 0rem;
-    display: block;
-    transition: all 0.2s ease-in-out;
-    background-color: #000000a3;
+    color: white;
+    height: 30px;
+    background-color: #00000060;
+    width: 100%;
+    overflow: hidden;
+    border: none;
+    &:hover {
+      border-bottom-left-radius: 1rem;
+      border-bottom-right-radius: 1rem;
+      border-top-left-radius: 0rem;
+      border-top-right-radius: 0rem;
+      display: block;
+      transition: all 0.2s ease-in-out;
+      background-color: #000000a3;
 
-    cursor: pointer;
-  }`;
+      cursor: pointer;
+    }
+  }
+`;
 
 ReactDOM.render(<VideoModal />, document.getElementById("root"));
